bbfd7e1ba85ec0717493be97d043f6ab51375e16
Ario98 <bliteum@protonmail.com>
2023-06-18 22:36:54 +0200

    Added first visualisation

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
 from sklearn import datasets
+import numpy as np
 import requests
 
 @st.cache_data
 def load_data():
-    iris = datasets.load_iris()
-    X = iris.data
-    y = iris.target
-    data = pd.DataFrame(X, columns=iris.feature_names)
-
-    gas_price_data = pd.read_csv('data/data_complete.csv')
+    data = pd.read_csv('data/data_complete.csv')
     return data
 
 class EthereumAnalysisApp:
@@ -19,7 +17,11 @@ class EthereumAnalysisApp:
     def __init__(self):
         st.title('Ethereum Gas Fees Analysis: Pre and Post London Upgrade')
         st.sidebar.image('https://ethereum.org/static/8ea7775026f258b32e5027fe2408c49f/57723/ethereum-logo-landscape-black.png', use_column_width=True)
-        self.page_selection = st.sidebar.selectbox("Choose a page", ["Homepage", "Pre Upgrade Data", "Post Upgrade Data", "Comparison"])
+        self.page_selection = st.sidebar.selectbox("Choose a page", ["Homepage", "Graphical Comparison", "Statistical Comparison"])
+
+        st.sidebar.markdown("Data by")
+        st.sidebar.image("https://etherscan.io/assets/svg/logos/logo-etherscan.svg?v=0.0.5", use_column_width=True)
+        st.sidebar.image("https://owlracle.info/img/owl.webp", width=50)
 
     def fetch_eth_price(self):
         url = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
@@ -71,29 +73,75 @@ class EthereumAnalysisApp:
         Feel free to navigate between these pages to explore the analysis. Each page will offer visualizations and insights about the data.
         """)
 
-    def display_pre_upgrade_data(self):
-        st.header("Pre Upgrade Data")
-        data = load_data()
-        st.write(data)
+    def graphical_comparison(self):
 
-    def display_post_upgrade_data(self):
-        st.header("Post Upgrade Data")
         data = load_data()
-        st.write(data)
 
-    def display_comparison(self):
-        st.header("Comparison")
+        data['Date'] = pd.to_datetime(data['Date'])
+
+        st.header("Graphical Comparison")
+
+        st.subheader("Gas Price")
+        st.write("The average gas price (in Gwei) graph illustrates the fluctuation in gas prices over time, providing insights into the cost of Ethereum network transactions. This visual representation helps users analyze trends and make informed decisions based on historical gas price data.")
+            
+        # Select the gas price metric
+        selected_metric = st.selectbox("Select Gas Price Metric", ['GasPriceOpen', 'GasPriceClose', 'GasPriceLow', 'GasPriceHigh', 'average_gas_price'])
+
+        # Highlight the date of the update (August 5th, 2021)
+        update_date = pd.to_datetime('2021-08-05')
+
+        # Filter data after the update date
+        data_after_update = data[data['Date'] >= update_date]
+
+        # Calculate average gas price before and after the update
+        avg_gas_price_before_update = data[data['Date'] < update_date][selected_metric].mean()
+        avg_gas_price_after_update = data_after_update[selected_metric].mean()
+
+        # Determine the color for the average gas price text based on the price change
+        avg_gas_price_color = "red" if avg_gas_price_after_update > avg_gas_price_before_update else "green"
+
+        # Determine the arrow symbol based on the price change
+        arrow_symbol = "↑" if avg_gas_price_after_update > avg_gas_price_before_update else "↓"
+
+        # Display average gas price before and after the update
+        col1, col2 = st.columns(2)
+        with col1:
+            st.subheader(f"Average Gas Price Before Update ({selected_metric})")
+            st.markdown(f"<h2>{avg_gas_price_before_update:.2f} Gwei</h2>", unsafe_allow_html=True)
+        with col2:
+            st.subheader("Average Gas Price After Update")
+            st.markdown(f"<h2 style='color:{avg_gas_price_color}'>{avg_gas_price_after_update:.2f} Gwei {arrow_symbol}</h2>", unsafe_allow_html=True)
+
+        # Create the graph
+        fig = go.Figure()
+
+        # Add a trace for the selected metric
+        fig.add_trace(go.Scatter(x=data['Date'], y=data[selected_metric], mode='lines', name=selected_metric))
+
+        # Add a vertical line for the update date
+        fig.add_shape(type='line', x0=update_date, x1=update_date, y0=0, y1=data[selected_metric].max(),
+                    line=dict(color='orange', dash='dash'), name='Update Date')
+
+        # Configure the layout
+        fig.update_layout(title='Gas Price Analysis', xaxis_title='Date', yaxis_title='Gas Price (Gwei)',
+                        legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01))
+
+        # Show the graph
+        st.plotly_chart(fig)
+
+
+
+    def statistical_comparison(self):
+        st.header("Statistical Comparison")
         st.markdown("Here, we compare pre-upgrade and post-upgrade data.")
 
     def app_run(self):
         if self.page_selection == "Homepage":
             self.display_homepage()
-        elif self.page_selection == "Pre Upgrade Data":
-            self.display_pre_upgrade_data()
-        elif self.page_selection == "Post Upgrade Data":
-            self.display_post_upgrade_data()
-        elif self.page_selection == "Comparison":
-            self.display_comparison()
+        elif self.page_selection == "Graphical Comparison":
+            self.graphical_comparison()
+        elif self.page_selection == "Statistical Comparison":
+            self.statistical_comparison()
 
 
 if __name__ == "__main__":
